0a4b3fa7b204c794480ed26e2cc103f26bfb29de
Semen Dubina <semen@sam002.net>
2020-04-25 11:39:33 +0300

    Fix indexesOf with empty query

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.yield
 
 
 fun String?.indexesOf(query: String, ignoreCase: Boolean = true): List<Int> {
+    if (query.isEmpty()) return listOf<Int>()
     val rexp = if (ignoreCase) Regex("$query", RegexOption.IGNORE_CASE) else  Regex("$query")
     val matches = rexp.findAll(this.toString(), 0)
     val indices = emptyList<Int>().toMutableList()
